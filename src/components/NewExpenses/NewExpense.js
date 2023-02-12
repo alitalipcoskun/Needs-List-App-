eb8491d73be84fdeaa8f929cdc6fa2402a098200
Ali Talip COŞKUN <ali_talip_18@hotmail.com>
2023-02-12 21:31:24 +0300

    Added Button
    Added activation button for adding new expense

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import './new-expense.css';
 import NewExpenseCard from "../UI/NewExpenseCard";
 
 function NewExpense(props){
-
+    const [addingExpense, setAddingExpense] = useState(false);
+    function addingExpenseHandler(){
+        setAddingExpense(true);
+    }
+    let newExpense = (
+        <div className="new-expense__actions">
+            <button onClick={addingExpenseHandler}>Add New Expense</button>
+        </div>
+    )
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -12,9 +20,14 @@ function NewExpense(props){
         };
         props.onAddingExpense(expenseData);
     }
+    if(addingExpense){
+        newExpense = (
+            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+        )
+    }
 
     return(<NewExpenseCard className="new-expense">
-        <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+        {newExpense}
     </NewExpenseCard>)
 }
 
